ff201ce4660194b46707a06a72eb7234d5475ab9
DuongKien <ddk0705@gmail.com>
2018-12-04 01:55:48 +0700

    Create first api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,43 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('version', function () {
+    return response()->json([
+        'version' => "1.0"
+    ]);
+});
+
+Route::get('users', function () {
+    return response()->json([
+        "1" => [
+            "email" => "admin@gmail.com",
+            "name" => "David A",
+            "role" => "Administrator"
+        ],
+        "2" => [
+            "email" => "user1@gmail.com",
+            "name" => "Anna",
+            "role" => "User"
+        ],
+        "3" => [
+            "email" => "user2@gmail.com",
+            "name" => "Manis",
+            "role" => "User"
+        ]
+    ]);
+});
+
+Route::post('users', function () {
+    return response()->json([
+        "status" => "Success",
+        "message" => "User created successfully!"
+    ]);
+});
+
+Route::delete('users', function () {
+    return response()->json([
+        "status" => "Success",
+        "message" => "User deleted successfully!"
+    ]);
+});
